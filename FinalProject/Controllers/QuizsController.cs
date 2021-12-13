a0c911a413b9e9d15f35d9f9174d0a7d5300be59
bindiya patel <bindiyapatel7069@gmail.com>
2021-12-13 15:35:41 -0500

    functionality for quiz adde

---
+++ FinalProject/Controllers/QuizsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FinalProject.Data;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    public class QuizsController : Controller
+    {
+        private readonly FinalProjectContext _context;
+
+        public QuizsController(FinalProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Quizs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Quiz.ToListAsync());
+        }
+
+        // GET: Quizs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quiz = await _context.Quiz
+                .FirstOrDefaultAsync(m => m.QuizId == id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+            quiz.IsDelete = quiz.IsDelete == "1" ? "Deleted" : "Not Deleted";
+            quiz.Subject = _context.Subject.Single(m => m.SubjectCode == quiz.Subject).Name;
+            return View(quiz);
+        }
+
+        // GET: Quizs/Create
+        public IActionResult Create()
+        {
+            ViewBag.Subjects = new SelectList(_context.Subject.ToList(), "SubjectCode", "Name");
+            return View();
+        }
+
+        // POST: Quizs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("QuizId,QuizTitle,Subject,QuizCode,IsDelete")] Quiz quiz)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(quiz);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quiz);
+        }
+
+        // GET: Quizs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quiz = await _context.Quiz.FindAsync(id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Subjects = new SelectList(_context.Subject.ToList(), "SubjectCode", "Name");
+            return View(quiz);
+        }
+
+        // POST: Quizs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("QuizId,QuizTitle,Subject,QuizCode,IsDelete")] Quiz quiz)
+        {
+            if (id != quiz.QuizId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(quiz);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuizExists(quiz.QuizId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quiz);
+        }
+
+        // GET: Quizs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quiz = await _context.Quiz
+                .FirstOrDefaultAsync(m => m.QuizId == id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+
+            return View(quiz);
+        }
+
+        // POST: Quizs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var quiz = await _context.Quiz.FindAsync(id);
+            _context.Quiz.Remove(quiz);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuizExists(int id)
+        {
+            return _context.Quiz.Any(e => e.QuizId == id);
+        }
+    }
+}
