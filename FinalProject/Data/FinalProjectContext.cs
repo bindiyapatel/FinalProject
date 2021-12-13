eb67a260d6f000285db997eb02a2b7a710e5c84f
bindiya patel <bindiyapatel7069@gmail.com>
2021-12-13 15:35:01 -0500

    Functionaliy for subject added

---
+++ FinalProject/Data/FinalProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FinalProject.Models;
+
+namespace FinalProject.Data
+{
+    public class FinalProjectContext : DbContext
+    {
+        public FinalProjectContext (DbContextOptions<FinalProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FinalProject.Models.Subject> Subject { get; set; }
+
+        public DbSet<FinalProject.Models.Quiz> Quiz { get; set; }
+
+        public DbSet<FinalProject.Models.Question> Question { get; set; }
+
+        public DbSet<FinalProject.Models.User> User { get; set; }
+    }
+}
